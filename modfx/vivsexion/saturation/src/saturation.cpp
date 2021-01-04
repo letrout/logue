46c5a4de8c3c061d48dce46077ac621ad0b314e4
Joel Luth <joel.luth@dell.comm>
2021-01-03 21:37:53 -0600

    convert lfo code to saturation

--- modfx/vivsexion/saturation/src/saturation.cpp
+++ modfx/vivsexion/saturation/src/saturation.cpp
@@ -11,34 +11,25 @@
 
 #include "usermodfx.h"
 
-#include "simplelfo.hpp"
+#include "saturation.hpp"
 
-static dsp::SimpleLFO s_lfo;
+static vivsexion::Saturation s_sat;
 
 enum {
-  k_sin = 0,
-  k_tri,
-  k_saw,
-  k_sqr,
-  k_sin_uni,
-  k_tri_uni,
-  k_saw_uni,
-  k_sqr_uni,
-  k_sin_off,
-  k_tri_off,
-  k_saw_off,
-  k_sqr_off,
-  k_wave_count
+  tanh,
+  sigmoid_bipolar,
+  hard_clip,
+  saturator_count
 };
 
-static uint8_t s_lfo_wave;
+static uint8_t s_saturator;
 static float s_param_z, s_param;
 static const float s_fs_recip = 1.f / 48000.f;
 
 void MODFX_INIT(uint32_t platform, uint32_t api)
 {
-  s_lfo.reset();
-  s_lfo.setF0(220.f,s_fs_recip);
+  // s_lfo.reset();
+  // s_lfo.setF0(220.f,s_fs_recip);
 }
 
 void MODFX_PROCESS(const float *main_xn, float *main_yn,
@@ -56,67 +47,31 @@ void MODFX_PROCESS(const float *main_xn, float *main_yn,
 
     p_z = linintf(0.002f, p_z, p);
     
-    s_lfo.cycle();
+    // s_lfo.cycle();
 
-    float wave;
+    float sat;
     
-    switch (s_lfo_wave) {
-    case k_sin:
-      wave = s_lfo.sine_bi();
+    switch (s_saturator) {
+    case tanh:
+      sat = s_sat.tanh(s_param_z, s_param);
       break;
       
-    case k_tri:
-      wave = s_lfo.triangle_bi();
+    case sigmoid_bipolar:
+      sat = s_sat.sigmoid_bipolar(s_param_z, s_param);
       break;
       
-    case k_saw:
-      wave = s_lfo.saw_bi();
-      break;
-      
-    case k_sqr:
-      wave = s_lfo.square_bi();
-      break;
-      
-    case k_sin_uni:
-      wave = s_lfo.sine_uni();
-      break;
-
-    case k_tri_uni:
-      wave = s_lfo.triangle_uni();
-      break;
-
-    case k_saw_uni:
-      wave = s_lfo.saw_uni();
-      break;
-
-    case k_sqr_uni:
-      wave = s_lfo.square_uni();
-      break;
-
-    case k_sin_off:
-      wave = s_lfo.sine_bi_off(s_param_z);
-      break;
-
-    case k_tri_off:
-      wave = s_lfo.triangle_bi_off(s_param_z);
-      break;
-
-    case k_saw_off:
-      wave = s_lfo.saw_bi_off(s_param_z);
-      break;
-
-    case k_sqr_off:
-      wave = s_lfo.square_bi_off(s_param_z);
+    case hard_clip:
+      sat = s_sat.hard_clip(s_param_z, s_param);
       break;
     }
     
     // Scale down the wave, full swing is way too loud. (polyphony headroom)
-    wave *= 0.1f;
+    // wave *= 0.1f;
     
-    *(my++) = wave;
-    *(my++) = wave;
-    *(sy++) = wave;
-    *(sy++) = wave;
+    *(my++) = sat;
+    *(my++) = sat;
+    *(sy++) = sat;
+    *(sy++) = sat;
   }
 
   s_param_z = p_z;
@@ -128,7 +83,7 @@ void MODFX_PARAM(uint8_t index, int32_t value)
   const float valf = q31_to_f32(value);
   switch (index) {
   case k_user_modfx_param_time:
-    s_lfo_wave = si_roundf(valf * (k_wave_count - 1));
+    s_saturator = si_roundf(valf * (saturator_count - 1));
     break;
   case k_user_modfx_param_depth:
     s_param = valf;
