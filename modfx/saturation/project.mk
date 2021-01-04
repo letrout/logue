4ee6deea13f92f0a898b8414f4711eb1064cee02
Joel Luth <joel.luth@dell.comm>
2021-01-03 20:11:26 -0600

    project skel

---
+++ modfx/saturation/project.mk
@@ -0,0 +1,17 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+PROJECT = saturation
+
+UCSRC = 
+
+UCXXSRC = saturation.cpp
+
+UINCDIR =
+
+UDEFS =
+
+ULIB = 
+
+ULIBDIR =
